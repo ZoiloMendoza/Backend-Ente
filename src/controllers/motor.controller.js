72ef765edd10148c2fe040863456b630ef34c868
ZoyMdz <64116048+ZoiloMendoza@users.noreply.github.com>
2023-09-29 14:44:23 -0600

    Update motor.controller.js

--- src/controllers/motor.controller.js
+++ src/controllers/motor.controller.js
@@ -1,2 +1,15 @@
+import { pool } from '../db.js';
 
-export const getMotores = (req, res) => res.send('Opteniendo motores')
+export const getMotores = (req, res) => res.sent('obteniendo motores');
+
+export const createMotores = (req, res) => {
+  const { name, otroDato} = req.body;
+  pool.query('INSERT INTO tabla (name, salary) VALUES (?, ?)', [name, salary]);
+  res.send('Post success');
+};
+
+export const updateMotores = (req, res) => res.send('actualizando motor');
+
+export const deleteMotores = (req, res) => res.send('Eliminando motor');
+
+
