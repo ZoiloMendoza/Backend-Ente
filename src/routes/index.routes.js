1c21ae05a82e63e4e2eb02b8dd0f13c8009fde2f
ZoyMdz <64116048+ZoiloMendoza@users.noreply.github.com>
2023-09-29 14:52:18 -0600

    Update index.routes.js

---
+++ src/routes/index.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { pool } from '/db.js';
+
+const router = Router();
+
+router.get() => {
+  const peticion = 'aqui va una consulta a la base de datos';
+  res.json(peticion);
+};
+
+export default router;
