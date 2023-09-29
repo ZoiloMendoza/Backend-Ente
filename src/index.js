791d7c0c8542acbf1e8dab521a530948e141b289
ZoyMdz <64116048+ZoiloMendoza@users.noreply.github.com>
2023-09-29 15:02:37 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import motorRoutes from './routes/motor.routes.js'
+import motorRoutes from './routes/motor.routes.js';
+import indexRoutes from './routes/index.routes.js';
 const app = express();
 
-
-app.use(motorRoutes)
+app.use(indexRoutes);
+app.use(motorRoutes);
 
 app.listen(3000);
 console.log('Server running on port 3000');
